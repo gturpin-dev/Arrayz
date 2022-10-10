1d5ce4b4ceec1e33ab8db5a114ab93f9b105fa6b
Guillaume <guillaume.turpin45@gmail.com>
2022-10-10 14:00:42 +0200

    :sparkles: Arrayz implements the Countable interface

--- src/Arrayz.php
+++ src/Arrayz.php
@@ -6,14 +6,16 @@ namespace Gturpin\Arrayz;
  * Class Arrayz - A simple array manipulation class
  * @package Gturpin\Arrayz
  */
-class Arrayz {
+class Arrayz implements \Countable {
+// class Arrayz implements \Stringable {
+// class Arrayz implements \ArrayAccess, \IteratorAggregate, \Countable, \JsonSerializable, \Serializable, \ArrayAccess, \Stringable {
 
 	/**
 	 * The array to be manipulated
 	 *
 	 * @var array
 	 */
-	private array $array;
+	protected array $array;
 	
 	/**
 	 * @param mixed|array $array The array to model or the value to wrap
@@ -35,5 +37,36 @@ class Arrayz {
 		if ( $die ) {
 			die;
 		}
+	}
+
+	/**
+	 * A shortcut to dump the array and die
+	 *
+	 * @return void
+	 */
+	public function dd() {
+		$this->dump( true );
+	}
+
+	/**
+	 * Get the array
+	 *
+	 * @return array
+	 */
+	public function get_array() {
+		return $this->array;
+	}
+
+	public function __toString() {
+		return '<pre>' . print_r( $this->array, true ) . '</pre>';
+	}
+
+	/**
+	 * Count the number of elements in the array
+	 *
+	 * @return int
+	 */
+	public function count(): int {
+		return count( $this->array );
 	}
 }
